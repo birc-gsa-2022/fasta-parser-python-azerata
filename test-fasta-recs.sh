5305522f9b82dadbf5bdc5bc0c86620c147bf5cc
azerata <smedemarkhammer@gmail.com>
2022-08-29 13:20:24 +0200

    Good stuff

--- test-fasta-recs.sh
+++ test-fasta-recs.sh
@@ -2,9 +2,9 @@
 
 success=1
 for f in test-data/*.fa; do
-    if ! cmp -s <(./fasta-recs $f) $f-fasta-recs-expected; then
+    if ! cmp -s <(python src/fasta-recs.py $f) $f-fasta-recs-expected; then
         echo "./fasta-recs $f did not produce the expected output"
-        diff <(./fasta-recs $f) $f-fasta-recs-expected
+        diff <(./python src/fasta-recs.py $f) $f-fasta-recs-expected
         echo
         success=0
     fi
